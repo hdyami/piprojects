3a737ba934b55580fcaa8fa8f251b4e3f6aaf5d2
hdyami <hailu.d@gmail.com>
2017-03-22 20:04:42 +0000

    starting to play with pwm

--- playpwm.py
+++ playpwm.py
@@ -20,47 +20,65 @@ SLP = 13
 AIN2 = 6
 AIN1 = 5
 
+# initialize pwm so we only have to do ChangeDutyCycle later
 GPIO.setup(BIN1, GPIO.OUT)
-BIN1_pwm=GPIO.PWM(26,100)
-my_pwm.start(0)
+BIN1_pwm=GPIO.PWM(BIN1,100)
+BIN1_pwm.start(0)
 
 GPIO.setup(BIN2, GPIO.OUT)
-GPIO.setup(SLP, GPIO.OUT)
-GPIO.setup(AIN2, GPIO.OUT)
-GPIO.setup(AIN1, GPIO.OUT)
+BIN2_pwm=GPIO.PWM(BIN2,100)
+BIN2_pwm.start(0)
 
-def forward(seconds=0):
+GPIO.setup(SLP, GPIO.OUT)
+
+GPIO.setup(AIN2, GPIO.OUT)
+AIN2_pwm=GPIO.PWM(AIN2,100)
+AIN2_pwm.start(0)
+
+GPIO.setup(AIN1, GPIO.OUT)
+AIN1_pwm=GPIO.PWM(AIN1,100)
+AIN1_pwm.start(0)
+
+def forward(seconds=0, dutyCycle=0):
     print "forward"
     GPIO.output(SLP, GPIO.HIGH)
 
-    GPIO.output(BIN1, GPIO.HIGH)
+    BIN1_pwm.ChangeDutyCycle(dutyCycle)
     GPIO.output(BIN2, GPIO.LOW)
 
     GPIO.output(AIN2, GPIO.LOW)
-    GPIO.output(AIN1, GPIO.HIGH)
+    AIN1_pwm.ChangeDutyCycle(dutyCycle)
 
     time.sleep(seconds)
-    stop();
+    AIN1_pwm.stop()
+    BIN1_pwm.stop()
 
-def backward(seconds=0):
+    stopall();
+
+def backward(seconds=0, dutyCycle=0):
     print "backward"
-    GPIO.output(BIN1, GPIO.LOW)
-    GPIO.output(BIN2, GPIO.HIGH) 
-
     GPIO.output(SLP, GPIO.HIGH)
-    GPIO.output(AIN2, GPIO.HIGH)
+
+    GPIO.output(BIN1, GPIO.LOW)
+    BIN2_pwm.ChangeDutyCycle(dutyCycle)
+
+    AIN2_pwm.ChangeDutyCycle(dutyCycle)
     GPIO.output(AIN1, GPIO.LOW)
     time.sleep(seconds)
-    stop()
+    BIN2_pwm.stop()
+    AIN2_pwm.stop()
 
-def stop():
+    stopall()
+
+def stopall():
     print "stop"
     # Motor breaking
     # set SLP on, set AIN2 off and AIN1 off
+    GPIO.output(SLP, GPIO.LOW)
+
     GPIO.output(BIN1, GPIO.LOW)
     GPIO.output(BIN2, GPIO.LOW) 
 
-    GPIO.output(SLP, GPIO.HIGH)
     GPIO.output(AIN2, GPIO.LOW)
     GPIO.output(AIN1, GPIO.LOW)
 
@@ -114,9 +132,12 @@ def spinRight(seconds=0):
     stop()
 
 
-# forward(.5)
-# backward(.5)
-spinLeft(1.5)
-spinRight(1.5)
+forward(2.5, 35)
+backward(1.5, 55)
+
+
+
+# spinLeft(1.5)
+# spinRight(1.5)
 
 GPIO.cleanup()
